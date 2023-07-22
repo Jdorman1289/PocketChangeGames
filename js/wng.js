1d00363aa6203bfb2dd667d4adae67d45c59a7d5
Jesse <Jdorman1289@gmail.com>
2023-07-22 18:08:32 -0500

    render all game cards to dom

--- js/wng.js
+++ js/wng.js
@@ -16,8 +16,55 @@ const getGames = async () => {
     }
 }
 
+// (async () => {
+//     const allGamesListToDisplay = await getGames();
+//     // console.log(allGamesListToDisplay);
+//
+//
+//     allGamesListToDisplay.map(game => {
+//         let card = document.createElement('div');
+//
+//         card.innerHTML = `
+//   <div class="card col" style="width: 18rem;">
+//   <img src="${game.thumbnail}" class="card-img-top" alt="...">
+//   <div class="card-body">
+//     <h5 class="card-title">${game.title}</h5>
+//     <p class="card-text">${game.short_description}</p>
+//     <a href="${game.game_url}" class="btn btn-primary">See Game</a>
+//   </div>
+// </div>`
+//         return card;
+//     });
+//
+//     for (let game of allGamesListToDisplay) {
+//         document.querySelector('.all-games').appendChild(game);
+//     }
+//
+//
+// })()
 (async () => {
-    const gamesListToDisplay = await getGames();
-    console.log(gamesListToDisplay);
+    const allGamesListToDisplay = await getGames();
+    // console.log(allGamesListToDisplay);
 
+    // Store the HTML elements in an array.
+    const gameCards = allGamesListToDisplay.map(game => {
+        let card = document.createElement('div');
+
+        card.innerHTML = `
+  <div class="col card" style="width: 18rem;">
+  <img src="${game.thumbnail}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${game.title}</h5>
+    <p class="card-text">${game.short_description}</p>
+    <a href="${game.game_url}" class="btn btn-primary">See Game</a>
+  </div>
+</div>`;
+        return card;
+    });
+
+    // Append each card to the DOM.
+    const gameContainer = document.querySelector('.all-games');
+    gameCards.forEach(card => {
+        gameContainer.appendChild(card);
+    });
 })()
