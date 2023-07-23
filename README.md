775e8362101e229f131a6be4d7b1c4696524bf04
Jesse <Jdorman1289@gmail.com>
2023-07-22 19:23:26 -0500

    finish basic design, change name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WalletNiceGames
-WNG (Wallet Nice Games) is a website for finding your next favorite free to play game.
+# PocketChangeGames
+PCG (Pocket Change Games) is a website for finding your next favorite free to play game.
 
 <a  href="https://icons8.com/icon/42249/games">Games</a> icon by <a href="https://icons8.com">Icons8</a>
--- js/wng.js
+++ js/wng.js
@@ -16,55 +16,33 @@ const getGames = async () => {
     }
 }
 
-// (async () => {
-//     const allGamesListToDisplay = await getGames();
-//     // console.log(allGamesListToDisplay);
-//
-//
-//     allGamesListToDisplay.map(game => {
-//         let card = document.createElement('div');
-//
-//         card.innerHTML = `
-//   <div class="card col" style="width: 18rem;">
-//   <img src="${game.thumbnail}" class="card-img-top" alt="...">
-//   <div class="card-body">
-//     <h5 class="card-title">${game.title}</h5>
-//     <p class="card-text">${game.short_description}</p>
-//     <a href="${game.game_url}" class="btn btn-primary">See Game</a>
-//   </div>
-// </div>`
-//         return card;
-//     });
-//
-//     for (let game of allGamesListToDisplay) {
-//         document.querySelector('.all-games').appendChild(game);
-//     }
-//
-//
-// })()
+
 (async () => {
     const allGamesListToDisplay = await getGames();
     // console.log(allGamesListToDisplay);
+    const gameContainer = document.querySelector('.all-games');
+    let searchValue = document.querySelector("input").value;
 
     // Store the HTML elements in an array.
     const gameCards = allGamesListToDisplay.map(game => {
         let card = document.createElement('div');
 
         card.innerHTML = `
-  <div class="col card" style="width: 18rem;">
+  <div class="col card bg-secondary" style="width: 18rem;">
   <img src="${game.thumbnail}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${game.title}</h5>
     <p class="card-text">${game.short_description}</p>
-    <a href="${game.game_url}" class="btn btn-primary">See Game</a>
+    <a href="${game.game_url}" class="btn btn-dark">See Game</a>
   </div>
 </div>`;
         return card;
     });
 
     // Append each card to the DOM.
-    const gameContainer = document.querySelector('.all-games');
     gameCards.forEach(card => {
         gameContainer.appendChild(card);
     });
+
+
 })()
