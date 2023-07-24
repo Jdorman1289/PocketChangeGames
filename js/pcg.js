8c8eb225886828b82599cb0bde007d0f48319eb2
Jesse <Jdorman1289@gmail.com>
2023-07-24 09:44:14 -0500

    make basic search functionality TODO update to real time search functionality

--- js/pcg.js
+++ js/pcg.js
@@ -44,5 +44,22 @@ const getGames = async () => {
         gameContainer.appendChild(card);
     });
 
+    document.querySelector('.searchBTN').addEventListener('click', () => {
+        let searchValue = document.querySelector("input").value;
+        allGamesListToDisplay.forEach(game => {
+            if (game.title.toLowerCase() === searchValue.toLowerCase()) {
+                gameContainer.innerHTML = `
+  <div class="col card bg-secondary" style="width: 18rem;">
+  <img src="${game.thumbnail}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${game.title}</h5>
+    <p class="card-text">${game.short_description}</p>
+    <a href="${game.game_url}" class="btn btn-dark">See Game</a>
+  </div>
+</div>`;
+            }
+        })
+    })
+
 
 })()
